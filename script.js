b46cdb63fee226980fb8042cdd318c12cf2af03b
Micah T. Sereno <78523967+mimiseno@users.noreply.github.com>
2025-02-27 14:55:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const handleAPI = async (userText) => {
 const generatePromptSuggestions = async () => {
     const result = await modelYoru.generateContent({
         contents: [
-            { role: "user", parts: [{ text: "Generate some engaging questions for Yoru from valorant to answer. make the questions interesting for yoru. ask querky questions if youd like. dont add numbers before the questions, and  dont let your questions be too long" }] }
+            { role: "user", parts: [{ text: "Generate some engaging questions for Yoru from valorant to answer. make the questions interesting for yoru. ask querky questions if youd like. dont add numbers before the questions, and dont make your questions too long or too short" }] }
         ]
     });
 
